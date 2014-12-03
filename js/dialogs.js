6be889b1f791f6fa4e1ee1c6274969b2043d46d5
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2014-12-04 00:12:16 +0200

    open dialog fixed

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,11 +1,5 @@
 var main = angular.module("main");
 main.controller("DialogsController", function($scope, $location, $element, ngDialog, $http){
-    $scope.openMessages = function(dialog) {
-        $location.search("path", "messages");
-        $location.search("id", dialog.user.id);
-    };
-
     Utilities.loadDataToScope(window.location.origin + "//getDialogs", {}, $scope, $http)
-
     Utilities.applyStylesToHtml($element);
 });
