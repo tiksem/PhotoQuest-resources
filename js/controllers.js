a0bdd5ed6601b3ff19b8e51f2792ac79210576f5
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2014-12-12 12:02:19 +0200

    profile add friend button logic integrated

--- js/controllers.js
+++ js/controllers.js
@@ -33,5 +33,55 @@ ControllerUtils = {
         $scope.getDisplayDate = function(addingDate) {
             return Utilities.getDisplayDate(addingDate);
         }
+    },
+    initProfileButtons: function($scope, $http) {
+        $scope.addOrRemoveFriend = function(user, decline) {
+            var config = {
+                params: {
+                    id: user.id
+                }
+            };
+
+            var removeFriend = user.relation == "friend" || user.relation == "request_sent" || decline;
+            var url = window.location.origin +
+                (removeFriend ? "//removeFriend" : "//addFriend");
+
+            $http.get(url, config).success(function(data){
+                if(!data.error){
+                    if(removeFriend){
+                        if (!decline) {
+                            delete user.relation;
+                        } else {
+                            user.relation = "follows";
+                        }
+                    } else {
+                        if(user.relation == "request_received"){
+                            user.relation = "friend";
+                        } else {
+                            user.relation = "request_sent";
+                        }
+                    }
+                } else {
+                    console.error(data);
+                }
+            });
+        };
+
+        $scope.getToggleFriendStatusButtonName = function(user) {
+            var relation = user.relation;
+            if(relation == "friend"){
+                return "Remove friend";
+            } else if(relation == "request_sent") {
+                return "Cancel friend request"
+            } else if(relation == "request_received") {
+                return "Accept friend request"
+            } else if(relation == "follows") {
+                return "Unfollow"
+            } else if(relation == "followed") {
+                return "Accept friend request"
+            } else {
+                return "Add friend";
+            }
+        };
     }
 };
--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,9 @@
 var main = angular.module("main");
 main.controller("ProfileController", function($rootScope, $scope, ngDialog, $element, $http, $location){
     var userId = $location.search()["id"];
-    Http.loadUserToScope($scope, $http, userId);
+    Http.loadUserToScope($scope, $http, userId, function(){
+        $scope.user = $scope;
+    });
+    ControllerUtils.initProfileButtons($scope, $http);
     Utilities.applyStylesToHtml($element);
 });
