d5bc49fd05801b72dc58588c1a3ca25e4a62adee
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-02-13 02:33:33 +0200

    Incorrect scroll position while switching sorting order on people page, fixed

--- js/search_select.js
+++ js/search_select.js
@@ -3,9 +3,12 @@ angular.module('main')
         return {
             link: function(scope, element, attrs) {
                 element = $(element);
+                var hiddenA = $("<a style='display: none'></a>");
+                element.parent().append(hiddenA);
                 element.val(scope.getOrder());
                 element.on('change', function () {
                     var valueSelected = this.value;
+                    hiddenA.click();
                     document.location.href =
                         ControllerUtils.getAddSearchParamFunction($location, attrs.searchSelect, valueSelected)();
                     //$location.search(attrs.searchSelect, valueSelected);
