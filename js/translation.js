b144296ba0aae514e75d8de9140e72e2e948f899
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-02-18 15:36:36 +0200

    several petrovich integrations

--- js/translation.js
+++ js/translation.js
@@ -1,23 +1,41 @@
 TRANSLATION = {
     en : {
+        of: function(name, gender, what) {
+            return name + "'s " + what;
+        },
         friendsOf: function(name) {
             return name + "'s Friends";
+        },
+        photosOf: function(name) {
+            return name + "'s Photos";
+        },
+        photoquestsOf: function(name) {
+            return name + "'s Photoquests";
         },
         people: "People",
         photoquests: "Photoquests"
     },
     ru: {
-        friendsOf: function(name, gender) {
+        of: function(name, gender, what, whatEn) {
             var person = {
                 gender: gender ? 'male' : 'female',
                 first: name
             };
             var result = petrovich(person, 'genitive').first.split("-")[0];
             if(result ===  name){
-                return TRANSLATION.en.friendsOf(name);
+                return TRANSLATION.en.of(name, gender, whatEn);
             }
 
-            return "Друзья " + result;
+            return what + " " + result;
+        },
+        friendsOf: function(name, gender) {
+            return this.of(name, gender, "Друзья", "Friends");
+        },
+        photosOf: function(name, gender) {
+            return this.of(name, gender, "Фотографии", "Photos");
+        },
+        photoquestsOf: function(name, gender) {
+            return this.of(name, gender, "Фотоквесты", "Photoquests");
         },
         people: "Люди",
         photoquests: "Фотоквесты"
