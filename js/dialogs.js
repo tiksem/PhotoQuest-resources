330a7e82c51a6ce90351273bb4dd4cbbb5704d34
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-02-19 17:56:25 +0200

    dialogs loadings fixed

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,5 +1,12 @@
 var main = angular.module("main");
-main.controller("DialogsController", function($scope, $location, $element, ngDialog, $http){
-    Utilities.loadDataToScope(window.location.origin + "//getDialogs", {}, $scope, $http)
+main.controller("DialogsController", function($scope, $timeout, $location, $element, ngDialog, $http){
+    Utilities.loadDataToScope(window.location.origin + "//getDialogs", {}, $scope, $http);
+
+    PhotoquestUtils.initPagination($scope, $http, $location, $element, $timeout, {
+        url: "//getDialogs",
+        countUrl: "//getDialogsCount",
+        scopeArrayName: "dialogs"
+    });
+
     Utilities.applyLinksBehavior($location, $scope, $element)();
 });
