0cab7cab3e67bce372476149fa80a867a6826490
dzhus <djus_mix@ukr.net>
2015-02-25 22:12:06 +0200

    translate login, registration pages and user menu

--- js/translation.js
+++ js/translation.js
@@ -13,7 +13,40 @@ TRANSLATION = {
             return name + "'s Photoquests";
         },
         people: "People",
-        photoquests: "Photoquests"
+        photoquests: "Photoquests",
+        profile: "Profile",
+        messages: "Messages",
+        friends: "Friends",
+        myPhotoquests: "My photoquests",
+        myPhotos: "My photos",
+        news: "News",
+        replies: "Replies",
+        settings: "Settings",
+        signOut: "Sign out",
+        login: "Login",
+        password: "Password",
+        signIn: "Sign in",
+        register: "Register",
+        byName: "By name",
+        country: "country",
+        city: "City",
+        sortBy: "Sort by",
+        gender: "Gender",
+        search: "Search",
+        regDate: "Registration date",
+        rating: "Rating",
+        selectGender: "Select gender",
+        male: "Male",
+        female: "Female",
+        registration: "Registration",
+        name: "Name",
+        lastName: "Last name",
+        code: "Security code"
+        
+        
+        
+        
+        
     },
     ru: {
         of: function(name, gender, what, whatEn) {
@@ -38,6 +71,34 @@ TRANSLATION = {
             return this.of(name, gender, "Фотоквесты", "Photoquests");
         },
         people: "Люди",
-        photoquests: "Фотоквесты"
+        photoquests: "Фотоквесты",
+        profile: "Профайл",
+        messages: "Сообщения",
+        friends: "Друзья",
+        myPhotoquests: "Мои фотоквесты",
+        myPhotos: "Мои фотографии",
+        news: "Новости",
+        replies: "Ответы",
+        settings: "Настройки",
+        signOut: "Выход",
+        login: "Логин",
+        password: "Пароль",
+        signIn: "Вход",
+        register: "Регистрация",
+        byName: "Имя",
+        country: "Страна",
+        city: "Город",
+        sortBy: "Сортировка",
+        gender: "Пол",
+        search: "Поиск",
+        regDate: "Дата регистрации",
+        rating: "Рейтинг",
+        selectGender: "Выберите пол",
+        male: "Мужской",
+        female: "Женский",
+        registration: "Регистрация",
+        name: "Имя",
+        lastName: "Фамилия",
+        code: "Защитный код"
     }
 };
