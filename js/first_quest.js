2e7912f97e8b039a626d4b6be38e77bb2f42f2a3
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-03-02 18:43:45 +0200

    first_quest.html loading

--- js/first_quest.js
+++ js/first_quest.js
@@ -2,8 +2,10 @@ var main = angular.module("main");
 main.controller("FirstQuest", function($location, $cookies, $timeout, $scope, $element, $http, $upload){
     $scope.perform = function() {
         var url = "//changeAvatar";
+        $scope.laoding = true;
         Utilities.uploadPhoto($scope, $upload, url, {}, function() {
             Http.trySignInFromCookies($cookies, $timeout, $scope, $http, function() {
+                $scope.laoding = false;
                 document.location.href = "#?path=quests";
             })
         });
