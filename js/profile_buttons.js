d937f77708bf7366a678a7856165402c4946b64a
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-03-07 19:05:46 +0200

    profile_buttons.html renamed

--- js/profile_buttons.js
+++ js/profile_buttons.js
@@ -2,6 +2,6 @@ angular.module('main')
     .directive('profileButtons', function() {
         return {
             scope: true,
-            templateUrl: 'html/profile_buttons.html'
+            templateUrl: 'html/user_buttons.html'
         };
     });
