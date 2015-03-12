f06e98942ca02fd90c4e84bedb5bae89ffd1aee6
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-03-12 14:58:49 +0200

    dialogs pagination fixed

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,7 +1,5 @@
 var main = angular.module("main");
 main.controller("DialogsController", function($scope, $timeout, $location, $element, ngDialog, $http){
-    Utilities.loadDataToScope(window.location.origin + "//getDialogs", {}, $scope, $http);
-
     PhotoquestUtils.initPagination($scope, $http, $location, $element, $timeout, {
         url: "//getDialogs",
         countUrl: "//getDialogsCount",
